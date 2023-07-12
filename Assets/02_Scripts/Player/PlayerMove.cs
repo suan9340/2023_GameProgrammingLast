af51aee0e208356dbef424d567735ab5ee9a52de
shin0411a@naver.com <shin0411a@naver.com>
2023-07-12 18:13:43 +0900

    플레이어 이동 수정

--- Assets/02_Scripts/Player/PlayerMove.cs
+++ Assets/02_Scripts/Player/PlayerMove.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
 public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 8.0f;
     private Rigidbody2D playerRigidbody;
 
+    // 미사일 프리팹
+    public GameObject missilePrefab;
+
+    // 미사일이 발사되는 순간의 속도
+    public float launchSpeed = 10.0f;
     private void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    void Start()
+    private void Start()
     {
-       
     }
 
     void Update()
@@ -21,33 +27,27 @@ public class PlayerMove : MonoBehaviour
         Move();
     }
 
-    private void Move()
-    {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(0.05f,0f,0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Translate(-0.05f, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Translate(0f, 0.05f, 0f);
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(0f, -0.05f, 0f);
-        }
+       
 
-        //RigidBody를 이용한 이동
+       void Move()
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(0.05f, 0f, 0f);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-0.05f, 0f, 0f);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(0f, 0.05f, 0f);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(0f, -0.05f, 0f);
+            }
 
-        //transform.Translate(Vector3.right * Time.deltaTime);
-        //transform.Translate(Vector3.right * Time.deltaTime);
-
-        //float xInput = Input.GetAxis("Horizontal");
-        //float yInput = Input.GetAxis("Vertical");
-        //Vector3 newVelocity = new Vector2(xInput * moveSpeed, yInput * moveSpeed);
-        //playerRigidbody.velocity = newVelocity;
-    }
+        }
+    
 }
---
+++ Assets/Bullet.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Bullet : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+    void Start()
+    {
+    
+    }
+ 
+}
